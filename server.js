a444e00991dcdc9a95a951ba1f58f8037a5ce314
Abner Matheus <abner.math.c@gmail.com>
2015-12-15 07:26:52 -0200

    Second commit

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 #!/bin/env node
 
+var express = require('express');
+var app = express();
 var http = require('http');
 
-http.createServer(function(req, res) {
-	
-	res.writeHeader(200, {'Content-Type' : 'text/plain'});
-	res.done('Hello World!');
-	
-}).listen(process.env.OPENSHIFT_NODEJS_PORT || 8080);
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
+
+http.createServer(app).listen(app.get('port') ,app.get('ip'), function () {
+    console.log("✔ Express server listening at %s:%d ", app.get('ip'),app.get('port'));
+    server();
+});
