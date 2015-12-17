10c9bbee0fdfae36c76ba34059bf4f4f42f808cf
Abner Matheus <abner.math.c@gmail.com>
2015-12-16 22:30:27 -0200

    Added list command[11]

--- src/commands/list.js
+++ src/commands/list.js
@@ -1,12 +1,11 @@
 module.exports = function(context, msg, matches) {
 	var chatId = msg.chat.id;
-	var type = matches[2] ? matches[2].toLowerCase().replace(/^\s+/, '') : '.*';
-	var keyword = matches[3] ? matches[3].toLowerCase().replace(/^\s+/, '') : '.*';
-	context.bot.sendMessage(chatId, chatId + ' ' + new RegExp('^' + context.utils.escapeRegExp(type.toLowerCase()) + '$', 'i') + ' ' + new RegExp(context.utils.escapeRegExp(keyword.toLowerCase()), 'i'));
+	var type = matches[2] ? context.utils.escapeRegExp(matches[2].toLowerCase().replace(/^\s+/, '')) : '.*';
+	var keyword = matches[3] ? context.utils.escapeRegExp(matches[3].toLowerCase().replace(/^\s+/, '')) : '.*';
 	context.mongo.label.find({
 		'chatId': chatId,
-		'type' : new RegExp('^' + context.utils.escapeRegExp(type.toLowerCase()) + '$', 'i'),
-		'label' : new RegExp(context.utils.escapeRegExp(keyword.toLowerCase()), 'i')
+		'type' : new RegExp('^' + type + '$', 'i'),
+		'label' : new RegExp(keyword.toLowerCase(), 'i')
 	}).toArray(function(err, arr) {
 		if (err) {
 			context.bot.sendMessage(chatId, context.vocabulary.sendError(err));
@@ -14,7 +13,6 @@ module.exports = function(context, msg, matches) {
 			if (arr.length == 0) {
 				context.bot.sendMessage(chatId, context.vocabulary.notFound());
 			} else {
-				context.bot.sendMessage(chatId, 'hmmmm' + arr.length);
 				context.addLastResult(chatId, {
 					'content' : arr,
 					'offset' : 0
