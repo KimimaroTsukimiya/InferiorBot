05d2763719a2170fce4fd2a5bf0d2a8e5a40f6f2
Abner Matheus <abner.math.c@gmail.com>
2015-12-16 22:27:17 -0200

    Added list command[10]

--- src/commands/list.js
+++ src/commands/list.js
@@ -2,6 +2,7 @@ module.exports = function(context, msg, matches) {
 	var chatId = msg.chat.id;
 	var type = matches[2] ? matches[2].toLowerCase().replace(/^\s+/, '') : '.*';
 	var keyword = matches[3] ? matches[3].toLowerCase().replace(/^\s+/, '') : '.*';
+	context.bot.sendMessage(chatId, chatId + ' ' + new RegExp('^' + context.utils.escapeRegExp(type.toLowerCase()) + '$', 'i') + ' ' + new RegExp(context.utils.escapeRegExp(keyword.toLowerCase()), 'i'));
 	context.mongo.label.find({
 		'chatId': chatId,
 		'type' : new RegExp('^' + context.utils.escapeRegExp(type.toLowerCase()) + '$', 'i'),
