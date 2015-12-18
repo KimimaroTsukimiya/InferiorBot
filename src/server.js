327e938b13883f53a37f07536cf547fa2301921a
Abner Matheus <abner.math.c@gmail.com>
2015-12-18 21:56:04 -0200

    ...

--- src/server.js
+++ src/server.js
@@ -24,7 +24,7 @@ var bot = new TelegramBot(token, {polling: true});
 console.log("Connecting to telegram bot with token " + token);
 
 var manager = new BotManager(bot, mongo, require('./vocabulary'), require('./utils'));
-manager.setReservedWords('help', 'ajuda', 'grava', 'salva', 'guarda', 'remove', 'deleta', 'lista');
+/*manager.setReservedWords('help', 'ajuda', 'grava', 'salva', 'guarda', 'remove', 'deleta', 'lista');
 manager.addCommand(/^\/(help|ajuda)(@inferiorbot)?$/i, '/help - Mostra essa lista deplorável.', require('./commands/help'));
 manager.addCommand(/^\/(grava|salva|guarda)(@inferiorbot)?\s+(\w+)$/i, '/salva <Rótulo> - Salva a última mensagem enviada sob um rótulo.', require('./commands/save-label'));
 manager.addCommand(/^\/(\w+)(@inferiorbot)?(\W.+)$/i, '/<Rótulo> <Frase> - Salva uma frase relacionada a um rótulo.', require('./commands/save-phrase'));
@@ -32,7 +32,7 @@ manager.addCommand(/^\/(\w+)(@inferiorbot)?$/i, '/<Rótulo> - Encaminha uma mens
 manager.addCommand(/^\/(remove|deleta)(@inferiorbot)?$/i, '/remove - Remove a última mensagem enviada por mim.', require('./commands/delete-label'));
 manager.addCommand(/^\/(remove|deleta)(@inferiorbot)?\s+(\w+)(\W.+)$/i, '/remove <Rótulo> <Frase> - Remove uma frase associada a um rótulo.', require('./commands/delete-phrase'));
 manager.addCommand(/^\/lista(@inferiorbot)?(\s+frase|\s+voz|\s+video|\s+imagem|\s+sticker|\s+texto)?(\s+.+)?$/i, '/lista <frase|voz|video|imagem|sticker|texto> <palavra-chave> - Lista os rótulos salvos.', require('./commands/list'));
-manager.init();
+manager.init();*/
 
 // Setup express
 app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8081);
